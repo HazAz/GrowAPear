82bfac82e35f5527a637099f99e08dec6cb56fcc
Hazem Aziz <h.hesham10@hotmail.com>
2023-12-09 05:51:02 -0500

    no message

---
+++ GrowAPear/Assets/scripts/MosquitoBulletScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MosquitoBulletScript : MonoBehaviour
+{
+	[SerializeField] private float minSpeed = 4f;
+	[SerializeField] private float maxSpeed = 7f;
+
+	private Vector3 targetPosition;
+	private float speed = 0f;
+
+	public void Init(Vector3 positionToTarget)
+	{
+		targetPosition = positionToTarget;
+		speed = Random.Range(minSpeed, maxSpeed);
+	}
+
+	private void Update()
+	{
+		transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (!other.CompareTag("Enemy"))
+		{
+			Destroy(gameObject);
+		}
+	}
+}
