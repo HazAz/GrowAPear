a3e4f3b5f9e5967ce1d12e76f7dfb1c8128172b9
DESKTOP-LSBSRKN\jayde <taliacazerez@gmail.com>
2023-12-09 10:11:31 -0600

    no message

--- GrowAPear/Assets/scripts/RestartButton.cs
+++ GrowAPear/Assets/scripts/RestartButton.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RestartButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnRestartButton()
     {
-        
+        SceneManager.LoadScene("Menu");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+
