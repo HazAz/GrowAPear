ede222d07f8d2eeb35b8c46c326aedb38c5f2b1d
Hazem Aziz <h.hesham10@hotmail.com>
2023-12-09 09:44:30 -0500

    die

--- GrowAPear/Assets/scripts/PlayerHealth.cs
+++ GrowAPear/Assets/scripts/PlayerHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -29,6 +30,6 @@ public class PlayerHealth : MonoBehaviour
 
     private void Die()
     {
-
-    }
+		SceneManager.LoadScene("GameOver");
+	}
 }
