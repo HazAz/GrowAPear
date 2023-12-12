0e23216049f04af30e79d85f3e599f266c625aad
Hazem Aziz <h.hesham10@hotmail.com>
2023-12-12 10:27:12 -0500

    final pass 2

--- GrowAPear/Assets/scripts/RestartLevel.cs
+++ GrowAPear/Assets/scripts/RestartLevel.cs
@@ -7,7 +7,8 @@ public class RestartLevel : MonoBehaviour
 {
     public void onRestartLevelButton()
     {
+        Debug.LogError(SceneManager.GetActiveScene().name);
         StaticPowerupScript.OnDeath();
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
